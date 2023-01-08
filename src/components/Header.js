95c73407b48120ff9732c76a24f9f1ca29a17049
Helen Taylor <helen.taylor@hotmail.it>
2023-01-08 10:16:47 +0000

    style Choirs page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import WellBeingLogo from "../images/wellbeing_logo.jpg";
 import { Navbar, Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import JingleBug from "../images/jinglebug.jpg";
+import GlowBug from "../images/glowbug.jpg";
 
 function Header() {
   return (
-    <Navbar bg="light" variant="light" expand="lg">
+    <Navbar bg="light" variant="light" expand="lg" style={{ border: "none" }}>
       <LinkContainer to="/">
         <Navbar.Brand>
           <img
@@ -24,10 +26,16 @@ function Header() {
             <Nav.Link>Home</Nav.Link>
           </LinkContainer>
           <LinkContainer to="/jinglebugs">
-            <Nav.Link>Jinglebugs</Nav.Link>
+            <Nav.Link>
+              <img src={JingleBug} height={20} />
+              Jinglebugs
+            </Nav.Link>
           </LinkContainer>
           <LinkContainer to="/glowbugs">
-            <Nav.Link>Glowbug Parties</Nav.Link>
+            <Nav.Link>
+              <img src={GlowBug} height={20} />
+              Glowbug Parties
+            </Nav.Link>
           </LinkContainer>
           <LinkContainer to="/wellbeingmusicclub">
             <Nav.Link>Wellbeing Music Club</Nav.Link>
